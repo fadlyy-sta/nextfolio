f56b960471dd8bade84e55f6a6f433a4e2e9ca03
fadlyy-sta <fadlypurnomo2@gmaill.com>
2025-10-21 16:08:23 +0700

    Update Section2.jsx

--- src/components/sections/Section2.jsx
+++ src/components/sections/Section2.jsx
@@ -3,7 +3,7 @@ export default function Section2() {
     <section id="about" className="min-h-[70vh]">
       {/* text about */}
       <div className="mb-12 flex flex-col gap-2">
-        <p className="text-center text-xl font-semibold">About me</p>
+        <p className="text-center text-xl font-semibold">Tentang Saya</p>
         <div className="flex items-center justify-center gap-3">
           <div className="h-1 w-15 border-b-2 border-black" />
           <div className="h-3 w-3 rotate-45 bg-black" />
@@ -17,7 +17,7 @@ export default function Section2() {
           <p className="text-xl font-semibold text-[#00b4d8]">I'm a Developer</p>
           <p className="mb-3 text-2xl font-bold capitalize">i can build amazing web applications.</p>
           <p className="italic">
-            Hello semuanya, Aku alfi, seorang developer yang berdedikasi untuk menciptakan solusi digital yang inovatif.
+            Hello semuanya, Aku Fadly, seorang yang berdedikasi untuk menciptakan solusi digital yang inovatif.
             memiliki pengalaman pada bidang web development membuat saya memiliki skill untuk mengembangkan aplikasi web
             yang responsif dan user-friendly. dan siap berkolaborasi dengan siapa saja
           </p>
